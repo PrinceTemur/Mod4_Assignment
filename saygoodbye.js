6bde6dc6db6e15072e94beef61e80592a1b16a43
PrinceTemur <66517000+PrinceTemur@users.noreply.github.com>
2020-06-06 16:44:55 +0530

    Add files via upload

---
+++ saygoodbye.js
@@ -0,0 +1,9 @@
+(function(){
+    
+    function speak(name) {
+    console.log(speakWord + " " + name);
+    }
+    
+})();
+var speakWord = "Good Bye";
+
---
+++ script.js
@@ -0,0 +1,9 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i=0;i<names.length;i++) {
+  if () {
+      
+  } else {
+    
+  }
+}
